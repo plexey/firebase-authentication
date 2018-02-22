a5d497e396a08ae250a86ef19319cc441368289e
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-22 20:15:06 +1100

    refactor: add styled component AppContainer refactor: add baseStyles applying css reset

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./Navigation";
+import baseStyles from "../basestyles";
 import LandingPage from "./Landing";
 import SignUpPage from "./SignUp";
 import SignInPage from "./SignIn";
@@ -9,24 +10,32 @@ import HomePage from "./Home";
 import AccountPage from "./Account";
 import * as routes from "../constants/routes";
 import withAuthentication from "./withAuthentication";
+import styled from 'styled-components';
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
-      <hr />
-      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
-      <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
-      <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
-      <Route
-        exact
-        path={routes.PASSWORD_FORGET}
-        component={() => <PasswordForgetPage />}
-      />
-      <Route exact path={routes.HOME} component={() => <HomePage />} />
-      <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
-    </div>
-  </Router>
-);
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const App = () => {
+  baseStyles();
+  return (
+    <Router>
+      <AppContainer>
+        <Navigation />
+        <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+        <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+        <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+        <Route
+          exact
+          path={routes.PASSWORD_FORGET}
+          component={() => <PasswordForgetPage />}
+        />
+        <Route exact path={routes.HOME} component={() => <HomePage />} />
+        <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
+      </AppContainer>
+    </Router>
+  );
+};
 
 export default withAuthentication(App);
