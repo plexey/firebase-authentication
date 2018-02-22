1bbca01b7794a71162adbf075bfc7d441230ddc0
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-22 22:00:17 +1100

    refactor: import Content and Content Heading styled components

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Content from '../components/Content';
+import ContentHeading from '../components/ContentHeading';
 
 const LandingPage = () =>
-  <div>
-    <h1>Landing Page</h1>
-  </div>
+  <Content>
+    <ContentHeading>Landing Page</ContentHeading>
+  </Content>
 
 export default LandingPage;
