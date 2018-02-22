67857c4dcbce3ff18fce653ba8b8d4c1507810ca
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-22 21:56:33 +1100

    feat: add styled component 'Button'

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: ${props => props.theme.brandColor};
+  color: ${props => props.theme.textColor};
+  border: none;
+  border-radius: 3px;
+  font-family: ${props => props.theme.font};
+  font-size: 16px;
+  padding: 10px 15px 10px 15px;
+  cursor: pointer;
+  font-weight: bold;
+
+  ${Button}:hover {
+    color: ${props => props.theme.brandColor};
+    background: ${props => props.theme.textColor};
+  }
+`;
+
+export default Button;
