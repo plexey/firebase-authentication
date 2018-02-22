75dc3bed3c229674e46d9cb16819c4c37f4ad536
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-22 17:33:30 +1100

    refactor: add NavigationAuth component refactor: add NavigationNonAuth component refactor: add SignOutButton component import

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import * as routes from '../constants/routes';
+import SignOutButton from "./SignOut";
+import * as routes from "../constants/routes";
 
-const Navigation = () =>
-  <div>
-    <ul>
-      <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-      <li><Link to={routes.LANDING}>Landing</Link></li>
-      <li><Link to={routes.HOME}>Home</Link></li>
-      <li><Link to={routes.ACCOUNT}>Account</Link></li>
-    </ul>
-  </div>
+const Navigation = (props, { authUser }) => (
+  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+Navigation.contextTypes = {
+  authUser: PropTypes.object,
+};
+
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={routes.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={routes.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={routes.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={routes.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={routes.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
+);
 
 export default Navigation;
