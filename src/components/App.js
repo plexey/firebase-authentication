e525b57b65740ed5c68b73ea9e672867245155c8
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-22 18:15:25 +1100

    refactor: remove unused imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./Navigation";
 import LandingPage from "./Landing";
@@ -8,7 +8,6 @@ import PasswordForgetPage from "./PasswordForget";
 import HomePage from "./Home";
 import AccountPage from "./Account";
 import * as routes from "../constants/routes";
-import { firebase } from "../firebase";
 import withAuthentication from "./withAuthentication";
 
 const App = () => (
