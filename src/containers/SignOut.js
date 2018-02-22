eb445fac9aaae650ff64317a19dfad0cf0db4be0
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-22 22:03:24 +1100

    refactor: add css rules

--- src/containers/SignOut.js
+++ src/containers/SignOut.js
@@ -1,13 +1,33 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import { auth } from '../firebase';
 
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  color: white;
+  font-size: 17px;
+  text-decoration: none;
+  background: none;
+  padding: 0 15px 0 15px;
+  font-weight: bold;
+  transition: 200ms ease all;
+  border: none;
+  font-family: ${props => props.theme.font};
+  cursor: pointer;
+
+  ${Button}:hover {
+    background: hsl(0, 0%, 20%);
+    color: ${props => props.theme.brandColor};
+  }
+`
+
 const SignOutButton = () =>
-  <button
+  <Button
     type="button"
     onClick={auth.doSignOut}
   >
     Sign Out
-  </button>
+  </Button>
 
 export default SignOutButton;
