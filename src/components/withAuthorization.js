8588fcde2ad21ddadd336481381228f041135a70
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-22 18:14:46 +1100

    refactor: connect component to redux store

--- src/components/withAuthorization.js
+++ src/components/withAuthorization.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { compose } from "recompose";
+import { withRouter } from "react-router-dom";
 
-import { firebase } from '../firebase';
-import * as routes from '../constants/routes';
+import { firebase } from "../firebase";
+import * as routes from "../constants/routes";
 
-const withAuthorization = (authCondition) => (Component) => {
+const withAuthorization = condition => Component => {
   class WithAuthorization extends React.Component {
     componentDidMount() {
       firebase.auth.onAuthStateChanged(authUser => {
-        if (!authCondition(authUser)) {
+        if (!condition(authUser)) {
           this.props.history.push(routes.SIGN_IN);
         }
       });
     }
 
     render() {
-      return this.context.authUser ? <Component /> : null;
+      return this.props.authUser ? <Component /> : null;
     }
   }
 
-  WithAuthorization.contextTypes = {
-    authUser: PropTypes.object,
-  };
+  const mapStateToProps = state => ({
+    authUser: state.sessionState.authUser
+  });
 
-  return withRouter(WithAuthorization);
-}
+  return compose(withRouter, connect(mapStateToProps))(WithAuthorization);
+};
 
 export default withAuthorization;
