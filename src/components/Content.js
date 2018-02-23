d8183a39bd4752ebb7a747c720f4e6c84c4d7b12
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-23 14:37:02 +1100

    style: add new rules

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,9 +3,15 @@ import styled from "styled-components";
 const Content = styled.div`
   grid-column-start: content-2;
   grid-column-end: content-3;
-  margin: 20px 0 0 0;
+  margin: 60px 0 60px 0;
   font-family: ${props => props.theme.font};
+  display: flex;
+  flex-direction: column;
   font-size: 16px;
+  background: white;
+  border-radius: 4px;
+  padding: 30px;
+  border: solid 2px hsl(0, 0%, 20%);
 `;
 
 export default Content;
