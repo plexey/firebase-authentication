76731d2764555d786a312985b489c145c01739b0
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-23 14:37:17 +1100

    style: tweak font size and margin

--- src/components/ContentHeading.js
+++ src/components/ContentHeading.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 const ContentHeading = styled.h1`
   color: ${props => props.theme.textColor};
   font-family: ${props => props.theme.font};
-  font-size: 24px;
+  font-size: 26px;
   font-weight: bold;
-  margin: 0 0 20px 0;
+  margin: 0 0 40px 0;
 `
 
 export default ContentHeading;
